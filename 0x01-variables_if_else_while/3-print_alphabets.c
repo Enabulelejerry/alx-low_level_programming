004be37ebbc292f0be481927c992629cfa9fa7b9
EnabuleleJerry <jerryenabulele@gmail.com>
2023-02-17 06:58:01 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,16 @@ int main(void)
 {
 	int ch;
 
-	for (ch='a'; ch<='z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n');
+	
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-
+	putchar('\n');
 	return (0);
 
 }
