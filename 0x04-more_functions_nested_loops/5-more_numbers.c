395304e6857e0cedc54faf910da6fa859ddcd15c
EnabuleleJerry <jerryenabulele@gmail.com>
2023-02-23 05:47:16 -0800

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
-
 void more_numbers(void)
 {
 	int i, j;
@@ -18,6 +17,6 @@ void more_numbers(void)
 				_putchar(j % 10 + '0');
 			}
 		}
-		putchar('\n');
 	}
+	_putchar('\n');
 }
